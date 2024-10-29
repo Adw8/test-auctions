d8c01031dd96667a60396ecd84747c46dbbcbe2b
Adwait Gharpure <69599306+Adw8@users.noreply.github.com>
2024-10-29 18:20:18 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://www.laconic.com/">Laconic!</a>
+          Hi from <a href="https://www.laconic.com/">Laconic!</a>
         </h1>
 
         <div className={styles.grid}>
